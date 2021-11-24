3f6f6ff8b1c7630848edea658f0cd528610f5103
spatel93 <sid.auburn@gmail.com>
2021-11-24 20:14:41 +0000

    convert query to parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,18 +9,19 @@ const config = {
 
 const pool = new Pool(config);
 const cohortName = process.argv[2];
-const limit = process.argv[3];
-
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit]
 pool.query(`
 select students.id, students.name, cohorts.name as cohort_name
 from students
 join cohorts on cohorts.id=cohort_id
-where cohorts.name like '${cohortName}%'
-limit ${limit}`)
+where cohorts.name like $1
+limit $2;`, values)
 .then(res => {
   res.rows.forEach( user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`)
   })
+  pool.end()
 })
 .catch((err)=> {
   console.error('query error', err.stack)
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,16 +9,15 @@ const config = {
 
 const pool = new Pool(config)
 const cohortName = process.argv[2];
-console.log(cohortName)
+const values = [cohortName]
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${cohortName || 'JUL02'}'
-ORDER BY teacher;`)
+pool.query(`SELECT teachers.name as teacher, cohorts.name as cohort 
+from teachers  
+JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id 
+JOIN students ON students.student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teachers.name;`, values)
 .then(res => {
   res.rows.forEach((result) => {
     console.log(`${result.cohort}: ${result.teacher}`);
